d9233ee4c244699eab92f6ed068e644dd234cfc1
sheikh-musa <sheikh.musa@outlook.com>
2022-06-17 11:27:56 +0800

    unsync

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = require("./routes");
 const { sequelize } = require("./db");
 
 const PORT = process.env.PORT || 3001;
-(async () => {
-	await sequelize.sync({ force: true });
-})();
+// (async () => {
+// 	await sequelize.sync({ force: true });
+// })();
 
 app.listen(PORT, () => {
 	console.log(`Listening to port ${PORT} ...`);
